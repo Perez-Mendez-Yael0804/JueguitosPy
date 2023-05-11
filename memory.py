6638a51e92fd8cba4cc01daf6a14ca27391da2a2
Perez-Mendez-Yael0804 <yperezm1900@alumno.ipn.mx>
2023-05-10 21:20:18 -0600

    Se centran los números en las casillas

--- memory.py
+++ memory.py
@@ -12,14 +12,12 @@ Exercises:
 
 from random import *
 from turtle import *
-
 from freegames import path
 
 car = path('car.gif')
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
-
 
 def square(x, y):
     """Draw white square with black outline at (x, y)."""
@@ -56,7 +54,6 @@ def tap(x, y):
         hide[mark] = False
         state['mark'] = None
 
-
 def draw():
     """Draw image and tiles."""
     clear()
@@ -74,13 +71,16 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        if tiles[mark] > 10:
+            goto(x + 5, y)
+        else:
+            goto(x + 16, y)
+
         color('black')
         write(tiles[mark], font=('Arial', 30, 'normal'))
 
     update()
     ontimer(draw, 100)
-
 
 shuffle(tiles)
 setup(420, 420, 370, 0)
